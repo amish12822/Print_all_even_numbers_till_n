8a8afbd5ec63e61e9d8738fd737d32793fbca713
amish12822 <amishofficial23@gmail.com>
2024-07-27 14:35:49 +0530

    complete program

---
+++ print_even.java
@@ -0,0 +1,15 @@
+/**
+ * print_even
+ */
+public class print_even {
+    public static void main(String[] args) {
+        int a = 11;
+
+        for( int i = 0; i <= a; i++){
+            if ( i%2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
